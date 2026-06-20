59296a1d1894399856da1084eb93170f46f950d3
Muxriddin <otkirovmuxriddin01@gmail.com>
2026-06-20 13:11:28 +0500

    fix: use dynamic env baseUrl with api prefix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,17 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // Vaqtincha .env dan emas, to'g'ridan-to'g'ri localhost deb yozib turamiz
-  baseURL: "http://localhost:5000/api", 
+  // Netlify'dagi VITE_API_URL havolasiga avtomatik /api ni qo'shib beradi
+  baseURL: import.meta.env.VITE_API_URL + "/api", 
 });
 
 export default API;
-// deploy qilish uchun cod
-// import axios from "axios";
-
-// const API = axios.create({
-//   // Qattiq yozilgan IP o'rniga .env dan o'qiydi
-//   baseURL: import.meta.env.VITE_API_URL + "/api", 
-// });
-
-// export default API;
